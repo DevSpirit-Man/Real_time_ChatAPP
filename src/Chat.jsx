5fa38856f7679c3a394734dbfad6405c09258815
lexarudev <ubuntudev@outlook.com>
2022-03-02 13:55:39 +0530

    commit

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import './css/chatbox.css'
 import './css/chatpage.css'
-import attach from './ico/attach.png'
-import menu from './ico/menu.png'
+import Button from '@mui/material/Button';
+// import attach from './ico/attach.png'
+// import menu from './ico/menu.png'
 import { useState } from 'react';
 import { db } from './firebase';
 import { addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
@@ -18,11 +19,11 @@ function Chat(props) {
         () => {
             onSnapshot(q, (snapshot) => setMessage(snapshot.docs.map((doc) => doc.data())))
         }
-        
-    ,[]);
-    useEffect(()=>{
-        updateScroll()
-    },message)
+
+        , [q]);
+    // useEffect(()=>{
+    //     updateScroll()
+    // },[message])
     async function sendMessage() {
         const msg = input;
 
@@ -30,50 +31,57 @@ function Chat(props) {
         await addDoc(collection(db, 'messages'), {
             name: props.name,
             text: msg,
+            userimg: props.photo,
             timestamp: serverTimestamp()
         });
-        updateScroll()
+        // updateScroll()
     }
-    function updateScroll(){
-        var element = document.getElementById("custom");
-        element.scrollTop = element.scrollHeight;
-    }
+    // function updateScroll(){
+    //     var element = document.getElementById("custom");
+    //     element.scrollTop = element.scrollHeight;
+    // }
     return (
         <div className='chatbox'>
             <div className="chat__header">
-                <div className="avatar__chat__header">
-                    <img src={props.photo} alt="" />
+                <div className="siginmenu" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginLeft: '-9px' }}>
+                    <div className="avatar__chat__header">
+                        <img src={props.photo} alt="" />
+                    </div>
+                    <div className="chat__header__name">
+                        <h5>{props.name}</h5>
+                    </div>
                 </div>
-                <div className="chat__header__name">
-                    <h5>{props.name}</h5>
-                </div>
-                <div className="chat__header__logosec">
+                {/* <div className="chat__header__logosec">
                     <img className='chat_header_ico' src={attach} alt="" />
                     <img className='chat_header_ico' src={menu} alt="" />
-                </div>
+                </div> */}
             </div>
 
 
 
             <div className="chat__body" id="custom">
-                    {
-                        message.map(item => {
-                            return (
-                                <div className="messageboxcont">
-                                    <div className="messagebox">
-                                        <p style={{ fontSize: '12px', fontWeight: 'bolder', color: '#3e573d' }}>{item.name}</p>
-                                        <p className="chat__body__message">
-                                            {item.text}
-                                        </p>
-                                    </div>
+                {
+                    message.map(item => {
+                        return (
+                            <div className="messageboxcont">
+                                <img style={{ width: '38px', borderRadius: '100%' }} src={item.userimg} alt="" />
+                                <div className="messagebox">
+                                    <h5 style={{ fontSize: '15px', fontWeight: '500' }}>{item.name}</h5>
+                                    <p className="chat__body__message">
+                                        {item.text}
+                                    </p>
                                 </div>
-                            )
-                        })
-                    }
+                            </div>
+                        )
+                    })
+                }
             </div>
             <div className="chat__footer">
                 <input value={input} type="text" placeholder='Type a message...' onChange={inputhandler} />
-                <button onClick={() => { sendMessage() }} style={{ width: '50px', height: '35px', background: '#86bbec', color: 'black', border: 'none', outline: 'none', borderRadius: '11px', fontWeight: 'bold', cursor: 'pointer' }}>send</button>
+                {/* <button onClick={() => { sendMessage() }} style={{ width: '79px', height: '35px', background: '#86bbec', color: 'black', border: 'none', outline: 'none', borderRadius: '11px', fontWeight: 'bold', cursor: 'pointer' }}>send</button> */}
+                <Button onClick={() => { sendMessage() }}  variant="outlined" style={{height:'35px'}} size="small">
+                    <p style={{fontWeight:'bold'}}>send</p>
+                </Button>
             </div>
         </div >
     );
--- src/Login.js
+++ src/Login.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import logo from './ico/logo.png'
-import { getAuth, signInWithPopup,  } from "firebase/auth";
+import { getAuth, signInWithPopup, } from "firebase/auth";
 import { provider } from './firebase';
+import Button from '@mui/material/Button';
+import gicon from './ico/gicon.png'
 export const Login = (props) => {
-    const loginstyle={
-        display:'flex',
-        flexDirection:'column',
-        width:'90px',
-        margin:'auto',
-        justifyContent:'center',
-        height:'80vh',
+    const loginstyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '90px',
+        margin: 'auto',
+        justifyContent: 'center',
+        alignItems:'center',
+        height: '70vh',
     }
-    async function senduserdetails(e){
+    async function senduserdetails(e) {
         e.preventDefault();
         const auth = getAuth();
-        const user=await signInWithPopup(auth, provider)
+        const user = await signInWithPopup(auth, provider)
         props.getuser(user.user)
     }
     return (
 
         <div className='login' style={loginstyle}>
-            <img src={logo} alt="" />
-            <h3 style={{margin:'15px 0'}}>ChatApp</h3>
-            <button style={{marginTop:'10px',backgroundColor:'black',color:'white',height:'36px',border:'none',outline:'none',width:'99px',cursor:'pointer',fontSize:'14px',borderRadius:'6px'}} onClick={senduserdetails}>LogIn</button>
+            <img style={{width:'120px'}} src={logo} alt="" />
+            {/* <h3 style={{margin:'15px 0',fontSize:'16px'}}>ChatApp</h3> */}
+            {/* <button style={{marginTop:'10px',backgroundColor:'black',color:'white',height:'36px',border:'none',outline:'none',width:'99px',cursor:'pointer',fontSize:'14px',borderRadius:'6px'}} >LogIn</button> */}
+            <Button sx={{ marginTop: '21px',width:'fit-content' }} onClick={senduserdetails} variant="outlined" size="large">
+                <img style={{width:'20px'}} src={gicon} alt="" />
+                <p style={{width:'190px',color:'#6e6d6d',fontWeight:'bold',marginLeft:'3px'}}>Login using Google</p>
+            </Button>
         </div>
     );
 };
