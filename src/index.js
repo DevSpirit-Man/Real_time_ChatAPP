5739cdf14060a37c9daeab08bc940542f4620f23
lexarudev <ubuntudev@outlook.com>
2022-04-07 11:55:57 +0530

    progressive web app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
