a9e803c7093f0c1fa5d6a3475b23c408ce6df1c9
lexarudev <ubuntudev@outlook.com>
2022-04-07 18:01:18 +0530

    store user and room id in localstorage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import './App.css';
 import Chat from './Chat'
-import { useState} from 'react';
+import { useEffect, useState} from 'react';
 import { Login } from './Login';
 import { Room } from './Room';
 import { Leftbar } from './Leftbar';
 function App() {
   const [user, setUser] = useState(null)
   const [room, setroom] = useState(null)
+useEffect(() => {
+  if(localStorage.getItem('user')){
+    setUser(JSON.parse(localStorage.getItem('user')))
+  }
+  if(localStorage.getItem('room')){
+    setroom(localStorage.getItem('room'))
+  }
+}, [])
 
   function setgetuser(e) {
     setUser(e)
+    localStorage.setItem('user',JSON.stringify(e))
   }
   function handlelogout() {
     setUser(null)
     setroom(null)
+    localStorage.removeItem('user')
+    localStorage.removeItem('room')
   }
   function roomfunc(e) {
     setroom(e)
+    localStorage.setItem('room',e)
   }
   function roomswitch(){
     setroom(null)
+    localStorage.removeItem('room')
   }
   return (
     <div className="App">
